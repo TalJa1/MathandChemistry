8148f8b0c280e9e21c95a7d2e9632b2707ee5a65
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-08-09 09:26:07 +0700

    refactor: Update solutionIcon function to include color parameter
    The solutionIcon function in svgXml.tsx has been updated to include a color parameter. This allows for dynamic coloring of the SVG icon. This refactor improves the flexibility and reusability of the solutionIcon function.

--- views/tabNavigator/docsNavigation/ReviewResult.tsx
+++ views/tabNavigator/docsNavigation/ReviewResult.tsx
@@ -1,29 +1,278 @@
 /* eslint-disable prettier/prettier */
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  Animated,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {containerStyle} from '../../../services/styleSheets';
+import {centerAll, containerStyle, vh, vw} from '../../../services/styleSheets';
 import useStatusBar from '../../../services/useStatusBarCustom';
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import {DataDetail} from '../../../services/typeProps';
+import {
+  backArrow,
+  examBack,
+  examNext,
+  saveIcon,
+  solutionIcon,
+} from '../../../assets/svgXml';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {Shadow} from 'react-native-shadow-2';
 
 const ReviewResult = () => {
-  useStatusBar('black');
+  useStatusBar('#A3A3F2');
   const route = useRoute();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const {data, userAnswer} = route.params as {
     data: DataDetail;
     userAnswer: string[];
   };
+  const [step, setStep] = useState(0);
 
   return (
     <SafeAreaView style={styles.container}>
+      <Header title="Review Result" step={step} total={data.test.length} />
       <ScrollView>
         <View>
           <Text>ReviewResult</Text>
         </View>
       </ScrollView>
+      <Shadow
+        distance={15}
+        startColor={'#FEFAAD33'}
+        endColor={'black'}
+        offset={[3, 4]}
+        style={{
+          marginTop: vh(1),
+          backgroundColor: 'black',
+          width: '100%',
+        }}>
+        <ExamNavigator
+          step={step}
+          setStep={setStep}
+          last={data.test.length}
+          reviewIndex={userAnswer.map((item, i) =>
+            item === data.test[i].correctAnswer[0] ? -1 : i,
+          )}
+          data={data}
+        />
+      </Shadow>
     </SafeAreaView>
+  );
+};
+
+const ExamNavigator: React.FC<{
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+  last: number;
+  reviewIndex: number[];
+  data: DataDetail;
+}> = ({step, setStep, last, reviewIndex, data}) => {
+  const [popUpVisible, setPopUpVisible] = useState(false);
+  const togglePopUp = () => {
+    setPopUpVisible(!popUpVisible);
+  };
+
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: vw(10),
+          paddingVertical: vh(2),
+        }}>
+        <TouchableOpacity
+          disabled={step === 0 ? true : false}
+          onPress={() => setStep(step - 1)}
+          style={[
+            {
+              backgroundColor: '#D2FD7C',
+              borderRadius: vw(20),
+              padding: vw(5),
+            },
+            centerAll,
+            step === 0 && {backgroundColor: '#464646'},
+          ]}>
+          {examBack(vw(7), vw(7), 'black')}
+        </TouchableOpacity>
+        <TouchableOpacity style={centerAll} onPress={() => togglePopUp()}>
+          {solutionIcon(vw(7), vw(7), '#FFFFFF')}
+          <Text style={{color: 'white', fontWeight: '500'}}>Xem cách giải</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setStep(step + 1)}
+          disabled={step === last - 1 ? true : false}
+          style={[
+            {
+              backgroundColor: '#D2FD7C',
+              borderRadius: vw(20),
+              padding: vw(5),
+            },
+            centerAll,
+            step === last - 1 && {backgroundColor: '#464646'},
+          ]}>
+          {examNext(vw(7), vw(7), 'black')}
+        </TouchableOpacity>
+      </View>
+      <PopUp
+        visible={popUpVisible}
+        onClose={togglePopUp}
+        data={data}
+        saveIndex={reviewIndex}
+      />
+    </View>
+  );
+};
+
+const {height: screenHeight} = Dimensions.get('window');
+
+const PopUp: React.FC<{
+  visible: boolean;
+  onClose: () => void;
+  data: DataDetail;
+  saveIndex: number[];
+}> = ({visible, onClose, data, saveIndex}) => {
+  const slideAnim = useRef(new Animated.Value(screenHeight)).current;
+  useEffect(() => {
+    if (visible) {
+      Animated.timing(slideAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true, // Set to true
+      }).start();
+    } else {
+      Animated.timing(slideAnim, {
+        toValue: screenHeight,
+        duration: 300,
+        useNativeDriver: true, // Set to true
+      }).start();
+    }
+  }, [visible, slideAnim]);
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <Animated.View
+      style={[styles.popUp, {transform: [{translateY: slideAnim}]}]}>
+      <View
+        style={{
+          justifyContent: 'space-between',
+          flex: 1,
+        }}>
+        <View
+          style={[
+            {backgroundColor: '#7EA8CA', width: vw(100), height: vh(10)},
+            centerAll,
+          ]}>
+          <Text style={styles.popUpText}>Các câu cần xem lại</Text>
+        </View>
+        <ScrollView
+          style={{flex: 1, paddingHorizontal: vw(5), paddingVertical: vh(2)}}>
+          {data.test.map((item, i) => (
+            <View key={i}>
+              {saveIndex.includes(i) && (
+                <View
+                  style={[
+                    {
+                      flexDirection: 'row',
+                      columnGap: vw(3),
+                      alignItems: 'flex-start',
+                      marginBottom: vh(2),
+                    },
+                  ]}>
+                  <Text
+                    style={{color: '#A3A3F2', fontSize: 16, fontWeight: '700'}}>
+                    Câu {i + 1}
+                  </Text>
+                  <Text style={{color: 'white'}}>{data.test[i].question}</Text>
+                </View>
+              )}
+            </View>
+          ))}
+        </ScrollView>
+        <View
+          style={{width: vw(100), alignItems: 'center', marginVertical: vh(2)}}>
+          <TouchableOpacity
+            onPress={onClose}
+            style={[
+              {
+                width: vw(70),
+                backgroundColor: '#D2FD7C',
+                height: vh(6),
+                borderRadius: 16,
+              },
+              centerAll,
+            ]}>
+            <Text style={{color: 'black', fontSize: 16, fontWeight: '500'}}>
+              Xem lại lần lượt
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Animated.View>
+  );
+};
+
+const Header: React.FC<{
+  title: string;
+  step: number;
+  total: number;
+}> = ({title, step, total}) => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <View
+      style={{
+        backgroundColor: '#A3A3F2',
+        padding: vw(5),
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        flexDirection: 'row',
+        columnGap: vw(5),
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        {backArrow(vw(7), vw(7), 'black')}
+      </TouchableOpacity>
+      <View style={{flex: 1}}>
+        <Text style={{color: '#0D0D0D', fontSize: 18, fontWeight: '600'}}>
+          {title}
+        </Text>
+      </View>
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            columnGap: vw(2),
+          }}>
+          <View
+            style={{
+              borderWidth: 2,
+              borderColor: '#565CE7',
+              borderRadius: vw(20),
+              paddingHorizontal: vw(3),
+              paddingVertical: vw(1),
+            }}>
+            <Text style={{color: '#0D0D0D', fontSize: 18, fontWeight: '600'}}>
+              {(step + 1).toString().padStart(2, '0')}/{total}
+            </Text>
+          </View>
+          <TouchableOpacity>
+            {saveIcon(vw(7), vw(7), '#565CE7')}
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 };
 
@@ -31,4 +280,26 @@ export default ReviewResult;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  popUp: {
+    position: 'absolute',
+    bottom: 0,
+    width: vw(100),
+    height: vh(100),
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    overflow: 'hidden',
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  popUpText: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#0D0D0D',
+  },
 });
