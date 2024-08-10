fe5b945a5c61929e956d8abf082e245c7bed36f4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-08-10 19:33:38 +0700

    refactor: Update Addition screen to use animated transition
    The Addition screen in Addition.tsx has been updated to use an animated transition when the screen is focused. This refactor improves the visual experience for the user by smoothly animating the screen elements into view.

--- views/tabNavigator/Addition.tsx
+++ views/tabNavigator/Addition.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import {Animated, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import React, {useRef} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
 import {
@@ -12,19 +12,31 @@ import {
   tradeIcon,
 } from '../../assets/svgXml';
 import {vh, vw} from '../../services/styleSheets';
+import {useFocusEffect} from '@react-navigation/native';
 
 const Addition = () => {
   useStatusBar('black');
+
+  const translateY = useRef(new Animated.Value(vh(100))).current;
+
+  useFocusEffect(
+    React.useCallback(() => {
+      Animated.timing(translateY, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+
+      return () => {
+        translateY.setValue(vh(100)); // Reset the animation value when the screen is unfocused
+      };
+    }, [translateY]),
+  );
+
   return (
     <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          flex: 1,
-          marginBottom: vh(4),
-          rowGap: vh(1),
-        }}>
+      <Animated.View
+        style={[styles.contentContainer, {transform: [{translateY}]}]}>
         <NavigationTab
           icon={groupIcon(vw(6), vw(6), '#D2FD7C')}
           label="Tạo đề"
@@ -49,7 +61,7 @@ const Addition = () => {
           icon={tradeIcon(vw(6), vw(6), '#D2FD7C')}
           label="Trao đổi"
         />
-      </View>
+      </Animated.View>
     </SafeAreaView>
   );
 };
@@ -77,5 +89,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
+  },
+  contentContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flex: 1,
+    marginBottom: vh(4),
+    rowGap: vh(1),
   },
 });
