d60d3e7381dd68f857b7a59b5c3141920efab9f6
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-08-12 16:10:31 +0700

    refactor: Update SetofQuestionCreation screen to include header and footer components
    The SetofQuestionCreation screen in SetofQuestionCreation.tsx has been updated to include a header and footer component. This refactor enhances the visual design and user experience of the screen by adding a styled header and footer.

--- views/tabNavigator/additionNavigation/SetofQuestionCreation.tsx
+++ views/tabNavigator/additionNavigation/SetofQuestionCreation.tsx
@@ -1,18 +1,105 @@
 /* eslint-disable prettier/prettier */
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {containerStyle} from '../../../services/styleSheets';
+import {containerStyle, vh, vw} from '../../../services/styleSheets';
+import {Shadow} from 'react-native-shadow-2';
 
 const SetofQuestionCreation = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <Shadow
+        distance={5}
+        startColor={'#5C5C5C'}
+        endColor={'black'}
+        offset={[3, 2]}
+        style={{
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          backgroundColor: 'black',
+          width: '100%',
+        }}>
+        <Header />
+      </Shadow>
       <ScrollView>
         <View>
           <Text>SetofQuestionCreation</Text>
         </View>
       </ScrollView>
+      <Shadow
+        distance={10}
+        startColor={'#5C5C5C'}
+        endColor={'black'}
+        offset={[3, 2]}
+        style={{
+          backgroundColor: 'black',
+          width: '100%',
+        }}>
+        <Footer />
+      </Shadow>
     </SafeAreaView>
+  );
+};
+
+const Footer: React.FC = () => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        height: vh(10),
+        alignItems: 'center',
+        paddingHorizontal: vw(5),
+      }}>
+      <TouchableOpacity
+        style={{
+          borderWidth: 1,
+          borderColor: '#D2FD7C',
+          backgroundColor: 'black',
+          width: '45%',
+          height: vh(7),
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 16,
+        }}>
+        <Text style={{color: '#D2FD7C', fontSize: 16, fontWeight: '500'}}>
+          Lưu
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        disabled={true}
+        style={{
+          backgroundColor: '#464646',
+          width: '45%',
+          height: vh(7),
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 16,
+        }}>
+        <Text style={{color: 'black', fontSize: 16, fontWeight: '500'}}>
+          Tạo đề
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <View
+      style={{alignItems: 'center', justifyContent: 'center', height: vh(8)}}>
+      <Text style={{color: 'white', fontSize: 16, fontWeight: '500'}}>
+        Tạo đề
+      </Text>
+    </View>
   );
 };
 
