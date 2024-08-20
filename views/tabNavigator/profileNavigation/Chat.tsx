bb30dcebef43a30afecb2f67d33e308caed886c3
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-08-20 14:44:38 +0700

    feat: Add input text component to Chat page

--- views/tabNavigator/profileNavigation/Chat.tsx
+++ views/tabNavigator/profileNavigation/Chat.tsx
@@ -1,36 +1,117 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Image,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
-import {containerStyle, vh, vw} from '../../../services/styleSheets';
+import React, {useState} from 'react';
+import {centerAll, containerStyle, vh, vw} from '../../../services/styleSheets';
 import useStatusBar from '../../../services/useStatusBarCustom';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation, useRoute} from '@react-navigation/native';
-import {cameraIcon, chatBackIcon, phoneIcon} from '../../../assets/svgXml';
+import {
+  attachmentIcon,
+  cameraIcon,
+  chatBackIcon,
+  chatCameraIcon,
+  chatNoteIcon,
+  micIcon,
+  phoneIcon,
+} from '../../../assets/svgXml';
 import {verticalContactData} from '../../../services/renderData';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {Shadow} from 'react-native-shadow-2';
 
 const Chat = () => {
   useStatusBar('black');
   const route = useRoute();
   const {index} = route.params as {index: number};
+  const [guest, setGuest] = useState([
+    'Xin chào',
+    'Có việc gì không vậy?',
+    'Gửi đề bài đi, biết đâu t giải được',
+  ]);
+
+  const [ownChat, setOwnChat] = useState([
+    'Hellooo',
+    'Tôi đang vướng một chút ở bài tập này',
+  ]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header index={index} />
-      <ScrollView>
-        <View>
-          <Text>Chat</Text>
+      <Shadow
+        distance={10}
+        startColor={'#191919'}
+        endColor={'black'}
+        offset={[3, 2]}
+        style={{
+          backgroundColor: 'black',
+          width: '100%',
+        }}>
+        <Header index={index} />
+      </Shadow>
+      <ScrollView
+        style={{paddingHorizontal: vw(5)}}
+        contentContainerStyle={{paddingVertical: vh(2)}}>
+        <View style={{alignItems: 'center'}}>
+          <Text
+            style={{
+              backgroundColor: '#464646',
+              paddingHorizontal: 10,
+              padding: 5,
+              borderRadius: vw(20),
+              color: '#FFFFFF',
+            }}>
+            Hôm nay
+          </Text>
         </View>
       </ScrollView>
+      <InputText setChat={setOwnChat} />
     </SafeAreaView>
+  );
+};
+
+const InputText: React.FC<{
+  setChat: React.Dispatch<React.SetStateAction<string[]>>;
+}> = ({setChat}) => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: vw(5),
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        columnGap: vw(2),
+        paddingVertical: vh(2),
+        borderTopWidth: 1,
+        borderTopColor: '#242E2E',
+      }}>
+      {attachmentIcon(vw(6), vw(6))}
+      <View style={[{flex: 1, flexDirection: 'row'}, centerAll]}>
+        <TextInput
+          placeholder="Write your message"
+          style={{
+            backgroundColor: '#192222',
+            borderRadius: 12,
+            paddingHorizontal: 10,
+            color: '#FFFFFF',
+            flex: 1,
+            paddingRight: 30,
+          }}
+        />
+        <View style={{position: 'absolute', right: 5}}>
+          {chatNoteIcon(vw(6), vw(6))}
+        </View>
+      </View>
+      {chatCameraIcon(vw(6), vw(6))}
+      {micIcon(vw(6), vw(6))}
+    </View>
   );
 };
 
@@ -70,8 +151,10 @@ const Header: React.FC<{index: number}> = ({index}) => {
           source={data.img}
         />
         <View>
-          <Text>{data.name}</Text>
-          <Text>Đang hoạt động</Text>
+          <Text style={{color: '#FFFFFF', fontSize: 16, fontWeight: '600'}}>
+            {data.name}
+          </Text>
+          <Text style={{color: '#797C7B'}}>Đang hoạt động</Text>
         </View>
       </View>
       {phoneIcon(vw(6), vw(6))}
